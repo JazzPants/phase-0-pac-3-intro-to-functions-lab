7ca6d54642039ff37ea9916d4fed1b49d52bf59b
JazzPants <leighton.l14@gmail.com>
2021-11-02 01:31:22 +1100

    Completed

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
+
+let string = "Hello"
+
+function shout(string) {
+    return string.toUpperCase();
+}
+
+function whisper(string) {
+    return string.toLowerCase();
+} /*function just performs the action/method, but you must 
+console.log(whisper(string))
+outside of the function if you want to see it */
+
+console.log(whisper(string));
+
+function logShout(string) {
+    console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+    console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(x) {
+    if (x === x.toLowerCase()) {
+        return ("I can't hear you!");
+    }
+    else if (x === x.toUpperCase()) {
+        return ("YES INDEED!");
+    }
+    else if (x === "I love you, Grandma.") {
+        return ("I love you, too.")
+    }
+}
+
+// 'Hello!'.toUpperCase(); -----> why doesn't this print out in terminal?
+
+    /*
+
 function shout(x){
     return x.toUpperCase();
 }
@@ -27,4 +66,5 @@ function sayHiToGrandma(x){
     }
     
 
-}
+*/
+
